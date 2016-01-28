25d0d233bb54021eedb5f6ce9e855b0fbd8147da
Laurel Schwulst <laurel.schwulst@gmail.com>
2016-01-28 13:08:43 -0600

    first commit

---
+++ lightbox.php
@@ -0,0 +1,51 @@
+<?php 
+
+/*
+ * Arena Document Example / Permalink
+ */
+
+include 'arena.php';
+
+$arena = new Arena();
+
+$item = $arena->get_block($_GET['id']);
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title><?= $channel->title; ?></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link rel="stylesheet" href="assets/css/app.css" type="text/css">
+  </head>
+  <body>
+
+    <div class="display">
+      <div class="theater">
+        <div class="stage" onclick="history.back()">
+          
+          <?php if($item->is_image()) { ?>
+            <a class='img' href="<?= $item->image_url('original') ?>">
+              <img src="<?= $item->image_url('display') ?>" />
+            </a>
+            <div class="title">
+              <h3><?= $item->generated_title ?></h3>
+              <?= $item->description_html ?>
+            </div>
+          <?php } ?>
+
+          <?php if($item->is_embed()){ ?>
+            <div class="blog-post-entry-content">
+              <?= $item->embed['html'] ?>
+            </div>
+            <div class="title">
+              <?= $item->description_html ?>
+            </div>
+          <?php } ?>
+
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
